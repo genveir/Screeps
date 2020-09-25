4a520f04990222edf7f66018d81f1584187ff778
Geerten Vink <maedaros@yahoo.com>
2020-09-25 11:08:14 +0200

    Harvester just has a CreepMemory now

--- src/roles/harvester.ts
+++ src/roles/harvester.ts
@@ -1,29 +1,16 @@
 import { Role } from "./roles";
 
-export interface HarvesterMemory extends CreepMemory {
-    role: string;
-    targets : HarvesterMemoryTargets;
-}
-
-export interface HarvesterMemoryTargets {
-    energySources : Id<Source>[];
-    index: number;
-    current: Id<Source>;
-}
-
 export class Harvester implements Role {
-    memory : HarvesterMemory;
-
     constructor(private creep : Creep)
     {
-        this.memory = <HarvesterMemory>creep.memory;
+
     }
 
     public run() {
         if (this.creep.store.getFreeCapacity() != 0) {
-            var target = Game.getObjectById(this.memory.targets.current)
+            var target = Game.getObjectById(this.creep.memory.targets.current)
 
-            if (!target) console.log("creep " + this.creep.name + " tried to go to id " + this.memory.targets.current + " which does not exist");
+            if (!target) console.log("creep " + this.creep.name + " tried to go to id " + this.creep.memory.targets.current + " which does not exist");
             else
             {
                 if (this.creep.harvest(target) == ERR_NOT_IN_RANGE) {
@@ -39,7 +26,7 @@ export class Harvester implements Role {
                 this.creep.moveTo(spawn);
             }
             else if (transferResult == ERR_FULL) {
-                (<HarvesterMemory>this.creep.memory).role = 'upgrader';
+                this.creep.memory.role = 'upgrader';
             }
             else
             {
@@ -50,20 +37,20 @@ export class Harvester implements Role {
 
     private updateTarget()
     {
-        var numTargets = this.memory.targets.energySources.length;
+        var numTargets = this.creep.memory.targets.energySources.length;
 
-        var currentTarget = this.memory.targets.index;
+        var currentTarget = this.creep.memory.targets.index;
 
         var newTarget = currentTarget + 1;
         if (newTarget >= numTargets) newTarget = 0;
 
-        (<HarvesterMemory>this.creep.memory).targets.index = newTarget;
-        (<HarvesterMemory>this.creep.memory).targets.current = this.memory.targets.energySources[newTarget];
+        this.creep.memory.targets.index = newTarget;
+        this.creep.memory.targets.current = this.creep.memory.targets.energySources[newTarget];
     }
 
-    public static initialMemory(energySources : Id<Source>[]) : HarvesterMemory
+    public static initialMemory(energySources : Id<Source>[]) : CreepMemory
     {
-        var mem : HarvesterMemory;
+        var mem : CreepMemory;
         mem = {
             role: 'harvester',
             targets: {
