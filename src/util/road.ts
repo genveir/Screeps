74c5d318900237cf6a1e7115319023470e0a0883
Geerten Vink <maedaros@yahoo.com>
2020-09-27 10:39:39 +0200

    some more ring road logic

--- src/util/road.ts
+++ src/util/road.ts
@@ -20,7 +20,7 @@ export class RoadUtil {
         var name = "spawnRing" + spawn.id;
 
         if (!room.memory.roads[name]) {
-            var route = this.getRingRoad(spawn.pos, 1);
+            var route = this._getRingRoad(spawn.pos, 1);
             room.memory.roads[name] = {start: spawn.pos, end: spawn.pos, route: route};
         }
         return room.memory.roads[name];
@@ -34,19 +34,27 @@ export class RoadUtil {
         var name = "controllerRing" + controller.id;
 
         if (!room.memory.roads[name]) {
-            var route = this.getRingRoad(controller.pos, 4);
+            var route = this._getRingRoad(controller.pos, 4);
             room.memory.roads[name] = {start : controller.pos, end: controller.pos, route: route};
         }
 
         return room.memory.roads[name];
     }
 
-    public static getRingRoad(center : SavedPosition, radius : number) : SavedPosition[] {
+    public static getRingRoad(center: SavedPosition, radius: number) : RoadDefinition {
+        var route = this._getRingRoad(center, radius);
+
+        return {start : center, end: center, route: route};
+    }
+
+    private static _getRingRoad(center : SavedPosition, radius : number) : SavedPosition[] {
         var route : SavedPosition[] = [];
         for (var y = -radius; y <= radius; y++) {
             for (var x = -radius; x <= radius; x++) {
                 if (x === -radius || x === radius || y === -radius || y === radius) {
-                    route.push(new RoomPosition(center.x + x, center.y + y, center.roomName));
+                    if (Game.rooms[center.roomName].getTerrain().get(center.x + x, center.y + y) !== TERRAIN_MASK_WALL) {
+                        route.push(new RoomPosition(center.x + x, center.y + y, center.roomName));
+                    }
                 }
             }
         }
