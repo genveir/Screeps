80a7cf480c31035a31dc12db5a2418ba3382c555
Geerten Vink <maedaros@yahoo.com>
2020-09-29 12:52:59 +0200

    part of the last commit

--- src/tasks/implementations/grab.ts
+++ src/tasks/implementations/grab.ts
@@ -79,7 +79,7 @@ export class Grab extends BaseTask implements Task {
             }
         }
 
-        if (this.getSuitability(creep) <= 0) this.unclaim(creep.id);
+        if (this.getPriority(creep) <= 0 || this.getSuitability(creep) <= 0) this.unclaim(creep.id);
     }
 
     public serialize() {
