c914df393d904ab8aabef419f73421f37e9b9aee
Geerten Vink <maedaros@yahoo.com>
2020-10-10 09:57:09 +0200

    adding memory interfaces to the typings

---
+++ src/types.d.ts
@@ -0,0 +1,5 @@
+interface FlagMemory { [name: string]: any }
+interface SpawnMemory { [name: string]: any }
+interface CreepMemory { [name: string]: any }
+interface RoomMemory { [name: string]: any }
+interface Memory { [name: string]: any }
