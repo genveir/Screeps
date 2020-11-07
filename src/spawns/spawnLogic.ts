11ad2696a3d647ee48c6feed634befc96e239010
Geerten Vink <maedaros@yahoo.com>
2020-11-07 22:11:36 +0100

    build workers as big as you can

--- src/spawns/spawnLogic.ts
+++ src/spawns/spawnLogic.ts
@@ -4,7 +4,7 @@ export class SpawnLogic
 
     }
 
-    private bodyParts = [WORK,CARRY,CARRY,MOVE,MOVE];
+    private workerParts = [WORK,CARRY,CARRY,MOVE,MOVE];
 
     public run() {
         var spawnRoom = this.spawn.room;
@@ -13,8 +13,58 @@ export class SpawnLogic
         
         if (!this.spawn.spawning) {
             if (creepcount < 14) {
-                this.spawn.spawnCreep(this.bodyParts, 'Harvester' + Game.time, { memory: {role : "harvester", slot: null} });
+                var spawnEnergy = this.spawn.store.getUsedCapacity(RESOURCE_ENERGY);
+                var extensionEnergy = this.spawn.room.find(FIND_STRUCTURES)
+                    .filter(s => s.structureType === STRUCTURE_EXTENSION)
+                    .map(s => <StructureExtension>s)
+                    .map(s => s.store.getUsedCapacity(RESOURCE_ENERGY))
+                    .reduce((previous, current) => previous + current);
+
+                var totalAvailableEnergy = spawnEnergy + extensionEnergy;
+
+                if (totalAvailableEnergy >= 300) {
+                    var body = this.buildBody(totalAvailableEnergy, this.workerParts);
+
+                    this.spawn.spawnCreep(body, 'Harvester' + Game.time, { memory: {role : "harvester", slot: null} });
+                }
             }
+        }
+    }
+
+    buildBody(maxEnergy : number, bodyParts: BodyPartConstant[]) : BodyPartConstant[] {
+        var index = 0;
+        var done = bodyParts.length === 0;
+        
+        var availableEnergy = maxEnergy;
+        var body : BodyPartConstant[] = [];
+        while (!done) {
+            var nextPart = bodyParts[index];
+            var cost = this.getPartCost(nextPart);
+
+            if (cost < availableEnergy) 
+            {
+                body.push(nextPart);
+                availableEnergy -= cost;
+            }
+            else done = true;
+
+            index++;
+            if (index == bodyParts.length) index = 0;
+        }
+
+        return body;
+    }
+
+    getPartCost(bpc : BodyPartConstant) : number {
+        switch(bpc) {
+            case MOVE: return 50;
+            case WORK: return 100;
+            case CARRY: return 50;
+            case ATTACK: return 80;
+            case RANGED_ATTACK: return 150;
+            case HEAL: return 250;
+            case CLAIM: return 600;
+            case TOUGH: return 10;
         }
     }
 }
